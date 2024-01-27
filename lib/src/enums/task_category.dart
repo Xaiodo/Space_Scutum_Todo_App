e1712d24e7264f72c185e0f939b16f5b0f1100a1
Xaiodo <rostislavgovbak@gmail.com>
2024-01-27 19:12:17 +0200

    Created task category adapter and changed task type id.

--- lib/src/enums/task_category.dart
+++ lib/src/enums/task_category.dart
@@ -1,2 +1,16 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+part 'task_category.g.dart';
+
 // This enum is used to filter tasks by category
-enum TaskCategory { all, home, work, personal }
+@HiveType(typeId: 1, adapterName: 'TaskCategoryAdapter')
+enum TaskCategory {
+  @HiveField(0)
+  all,
+  @HiveField(1)
+  home,
+  @HiveField(2)
+  work,
+  @HiveField(3)
+  personal,
+}
