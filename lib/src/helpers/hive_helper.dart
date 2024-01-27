2ea89d8ca039277d93b70686247998bb73d9c900
Xaiodo <rostislavgovbak@gmail.com>
2024-01-27 19:17:06 +0200

    Created task category adapter and changed task type id.

--- lib/src/helpers/hive_helper.dart
+++ lib/src/helpers/hive_helper.dart
@@ -1,5 +1,6 @@
 import 'package:hive_flutter/adapters.dart';
 
+import '../enums/task_category.dart';
 import '../models/task.dart';
 
 abstract class HiveHelper {
@@ -9,8 +10,12 @@ abstract class HiveHelper {
     // Initialize Hive
     await Hive.initFlutter();
 
-    // Register the adapter for Task
+    // Register the adapters for Task
+    Hive.registerAdapter<TaskCategory>(TaskCategoryAdapter());
     Hive.registerAdapter<Task>(TaskAdapter());
+
+    // Open the tasks box
+    await Hive.openBox<Task>(_boxName);
   }
 
   // Get the tasks box from Hive
